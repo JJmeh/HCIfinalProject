6e3336768e77a57a4a1585718a0dcb753c79514d
JJmeh <jeremiah.kaliki@gmail.com>
2021-06-04 11:22:21 +0700

    Added more pages

--- script.js
+++ script.js
@@ -2,23 +2,38 @@
 
 //testing
 
-var slideIndex = 0;
+// var slideIndex = 0;
 
-showSlides(slideIndex);
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function showSlides(n) {
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += "active";
-}
+// function showSlides(n) {
+//   var slides = document.getElementsByClassName("mySlides");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (var i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex-1].style.display = "block";
+// //   dots[slideIndex-1].className += "active";
+// }
 
+var curr = 1;
 
+$(function(){
+  $(".mySlides").hide();
+  $(".mySlides:nth-child(" + curr + ")").fadeToggle();
+  console.log("init");
+  $(".next").click(function(){
+    console.log($(".mySlides").length)
+    $(".mySlides:nth-child(" + curr + ")").hide();
+    curr++;
+    if (curr > $(".mySlides").length) {
+      curr = 1;
+    }
+    $(".mySlides:nth-child(" + curr + ")").fadeToggle();
+  })
+})
