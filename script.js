50a2610483b09801c38bfae141ba7f11d83724f4
JJmeh <jeremiah.kaliki@gmail.com>
2021-06-10 22:17:53 +0700

    Edited : 10 June 2021

--- script.js
+++ script.js
@@ -37,6 +37,16 @@ $(function(){
     $(".mySlides:nth-child(" + curr + ")").fadeToggle();
   })
 
+  $(".prev").click(function(){
+    // console.log($(".mySlides").length)
+    $(".mySlides:nth-child(" + curr + ")").hide();
+    curr--;
+    if (curr < 1) {
+      curr = $(".mySlides").length;
+    }
+    $(".mySlides:nth-child(" + curr + ")").fadeToggle();
+  })
+
   //navbar
   $(".hamburger-menu").click(function(){
     console.log("clicked")
